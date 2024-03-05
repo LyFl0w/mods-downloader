84bc0fde1b4e10d12a17a18c28cab6b176d9e6b0
LyFl0w <off.lyflow@gmail.com>
2024-03-05 19:42:15 +0100

    trying to convert modslist into curseforge modpack.zip

--- modpack-creator.py
+++ modpack-creator.py
@@ -1,79 +1,105 @@
-import requests
-import os
-import shutil
-import time
+from utils import *
 
-modsLoaderType =  {"Forge" : 1, "Cauldron" : 2, "LiteLoader" : 3, "Fabric" : 4, "Quilt" : 5, "NeoForge" : 6}
 api = "https://api.curse.tools/v1/cf"
 curse_forge_api = "https://www.curseforge.com/api/v1"
-counter_request = 0
-total_request = 0
 
+data_path = "data/"
+datapack_path = "datapack/"
 
-def request(path, params=None):
-    global counter_request, total_request
-
-    # module anti spam / ddos
-    if counter_request >= 1500:
-        counter_request = 0
-        time.sleep(1)
-
-    r = requests.get(path, params)
-    counter_request += 1
-    total_request += 1
-    return r.json()
-    
 
 def filter_on(files, path, contain_data):
     return [file for file in files if all(data in file[path] for data in contain_data)]
 
 
-def setup_target_mod(mods_id, mods_name, file_path):
-
-    def get_id(path, filter, params=None):
+def get_name_id(path, filter, params=None):
         r = request(f'{api}/{path}', params)
         data = r["data"]
         data = filter_on(data, "name", [filter])
         return data[0]["id"]
 
-    minecraft_id = get_id("games", "Minecraft")
-    mods_categorie_id = get_id("categories", "Mods", params={'gameId' : minecraft_id})
 
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
-        loader_info = lines[0][:-1].split(" ")
-        loader_id = modsLoaderType[loader_info[0]]
-        
-        for line in lines[1:]:
-            mod_name = line.split(" - ")[0]
-            index = 0
-            r = []
+mods_loader_type =  {"Forge" : 1, "Cauldron" : 2, "LiteLoader" : 3, "Fabric" : 4, "Quilt" : 5, "NeoForge" : 6}
+minecraft_id = get_name_id("games", "Minecraft")
+mods_categorie_id = get_name_id("categories", "Mods", params={'gameId' : minecraft_id})
+modpacks_categorie_id = get_name_id("categories", "Modpacks", params={'gameId' : minecraft_id})
+
+
+def mod_id_already_save(name):
+    pass
+
+
+def setup_target_mod(mods_id, mods_name, loader_id):
+    lines = read_file("mods.txt")
+    print(f'detected mods in file : {mods_number}')
+
+    for line in lines:
+        mod_name = line.split(" - ")[0]
+        index = 0
+        r = []
             
-            while len(r) == 0 and index + 50 <= 10_000:
-                r = request(f'{curse_forge_api}/mods/search', params={'gameId' : minecraft_id, 'classId' : mods_categorie_id, 'filterText': mod_name,
+        while len(r) == 0 and index + 50 <= 10_000:
+            r = request(f'{curse_forge_api}/mods/search', params={'gameId' : minecraft_id, 'classId' : mods_categorie_id, 'filterText': mod_name,
                                                             'gameFlavorId' : loader_id, 'index': index, 'sortField': 1})
-                if "data" not in r:
-                    break
+            if "data" not in r:
+                break
 
-                r = filter_on(r["data"], "name", mod_name)
-                index += 50
+            r = filter_on(r["data"], "name", mod_name)
+            index += 50
 
-            if isinstance(r, dict) or index + 50 > 10_000:
-                print(f"mod not found : {mod_name}")
-                print(r)
-                continue
+        if isinstance(r, dict) or index + 50 > 10_000:
+            print(f"mod not found : {mod_name}")
+            print(r)
+            continue
 
-            mod = r[0]
-            mod_id = mod['id']
+        mod = r[0]
+        mod_id = mod['id']
 
-            mods_id.append(mod_id)
-            mods_name[mod_id] = mod_name
-        
-        return loader_info, len(lines)-1
+        mods_id.append(mod_id)
+        mods_name[mod_id] = mod_name
+
+
+def setup_target_modpack(loader_id):
+    lines = read_file("modpacks.txt")
+    print(f'detected modpacks in file : {len(lines)}')
+
+    to_write = []
+
+    for line in lines:
+        modpack_name = line.split(" - ")[0]
+        index = 0
+        r = []
+            
+        while len(r) == 0 and index + 50 <= 10_000:
+            r = request(f'{curse_forge_api}/mods/search', params={'gameId' : minecraft_id, 'classId' : modpacks_categorie_id, 'filterText': modpack_name,
+                                                            'gameFlavorId' : loader_id, 'index': index, 'sortField': 1})
+            if "data" not in r:
+                break
+
+            r = filter_on(r["data"], "name", modpack_name)
+            index += 50
+
+        if "data" not in r or index + 50 > 10_000:
+            print(f"modpack not found : {modpack_name}")
+            print(r)
+            continue
+
+        modpack_id = r[0]['id']
+        dependencies = request(f'{curse_forge_api}/mods/{modpack_id}/dependencies', params={'type' : 'Include'})["data"]
+
+        for dependency in dependencies:
+            mod_id = dependency["id"]
+            mod_name = dependency["name"]
+            curse_forge_link = "https://www.curseforge.com/minecraft/mc-mods/"+dependency["slug"]
+            
+            
+            to_write.append(f'{mod_name} - {mod_id} - {curse_forge_link}')
+
+    
+    write_file(to_write, data_path, "infmods_modpacks.txt")
 
 
 def setup_mod_id(mods_id, require_depencies, incompatible_dependencies, mods_link, mods_name, loader_info):
-    loader_id = modsLoaderType[loader_info[0]]
+    loader_id = mods_loader_type[loader_info[0]]
     game_version = loader_info[1]
 
     def setup_mod(mod_id):
@@ -162,16 +188,10 @@ def check_incompatibility(mods_id, require_depencies, incompatible_dependencies)
     return check_result
 
 
-def write_links_to_file(links, file_path):
-    with open(file_path, 'w') as file:
-        for link in links:
-            file.write(link + '\n')
-
-
 if __name__ == "__main__":
-    # mods id contain in file
-    mods_id = []
-
+    # mods_id -> id - name - links
+    create_files_if_not_exist(data_path, ["mods_id.txt", "infmods.txt", "infmods_modpacks.txt"])
+    
     # id : string
     mods_name = {}
 
@@ -184,22 +204,25 @@ if __name__ == "__main__":
     # all mods links to download
     mods_link = []
 
-    loader_info, mods_number = setup_target_mod(mods_id, mods_name, "mods.txt")
+    loader_info = read_file("config.txt")[0].split(" ")
+    loader_info.append(mods_loader_type[loader_info[0]])
     print(f'detected info : {loader_info}')
-    print(f'detected mods in file : {mods_number}')
+
+    setup_target_mod(mods_id, mods_name, loader_info[2])
     print(f'detected mods by name on curseforge : {len(mods_id)}')
     print(f'names : {mods_name}')
-    print(f'total requests : {total_request}')
+
+    setup_target_modpack(loader_info)
 
     setup_mod_id(mods_id, require_depencies, incompatible_dependencies, mods_link, mods_name, loader_info)
     print(f'names : {mods_name}')
     print(f'total mods to download : {len(mods_link)}')
     print(f'require : {require_depencies}')
     print(f'incompatibility : {incompatible_dependencies}')
-    print(f'total requests : {total_request}')
+    print(f'\ntotal requests : {total_request}')
 
     if check_incompatibility(mods_id, require_depencies, incompatible_dependencies):
         print("S'il vous plait, mettez à jour votre liste de mods !")
         #exit(-1)
 
-    write_links_to_file(mods_link, "mods_link.txt")
+    write_file(mods_link, data_path, "infmods.txt")
---
+++ utils.py
@@ -0,0 +1,79 @@
+import requests
+import os
+import time
+from pathlib import Path
+import yaml
+import json
+
+counter_request = 0
+total_request = 0
+
+
+def delete_folder_if_exist(directory):
+    if os.path.exists(directory):
+        os.rmdir(directory)
+
+
+def delete_files_if_exist(files):
+    for file in files:
+        if os.path.exists(files):
+            os.remove(file)
+
+
+def create_files_if_not_exist(path=None, files=None):
+    start_path = ""
+    if path is not None:
+        start_path = path
+        
+    for file in files:
+        real_path = get_real_path(start_path, file)
+        if not os.path.exists(real_path):
+            write_file([], path, file)
+
+def get_real_path(path, file_name):
+    if path.endswith("/"):
+        return path+file_name
+    elif file_name.startswith("./"):
+        return path+file_name[1:]
+    else:
+        return path+"/"+file_name
+
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        return file.readlines()
+
+
+def read_yaml_file(file_path):
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
+
+def read_json_file(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def write_file(datas, path=None, file_name=None):
+    file_path = file_name
+    if path is not None:
+        Path(path).mkdir(parents=True, exist_ok=True)
+        file_path = get_real_path(path, file_name)
+
+    with open(file_path, 'w') as file:
+        for data in datas:
+            file.write(data + '\n')
+
+
+def request(path, params=None):
+    global counter_request, total_request
+
+    # module anti spam / ddos
+    if counter_request >= 1500:
+        counter_request = 0
+        time.sleep(1)
+
+    r = requests.get(path, params)
+    counter_request += 1
+    total_request += 1
+    return r.json()
