8e0d7f9e5f2583d68e7d09c6142a31b7430d43c3
LyFl0w <off.lyflow@gmail.com>
2024-03-09 23:55:45 +0100

    fix resourcepacks issue add classic mod folder

--- modpack-creator.py
+++ modpack-creator.py
@@ -1,3 +1,4 @@
+import urllib.request
 from utils import *
 
 api = "https://api.curse.tools/v1/cf"
@@ -140,6 +141,47 @@ def setup_target_mod(loader_id):
 
     if len(to_write) > 0:
         write_file(to_write, data_path, "mods_id.txt")
+
+
+def setup_target_resourcepacks():
+    lines = read_file("resourcepacks.txt")
+    print(f'detected resourcepacks in file : {len(lines)}')
+
+    to_write = []
+    for line in lines:
+        mod_name = line.split(f"{spliter}")[0]
+
+        if is_already_save(mod_name):
+            add_to_list(get_id_save(mod_name), mod_name, "resourcepack")
+            continue
+            
+        index = 0
+        r = []
+            
+        while len(r) == 0 and index + 50 <= 10_000:
+            r = request(f'{curse_forge_api}/mods/search', params={'gameId' : minecraft_id, 'classId' : resourcepacks_categorie_id, 'filterText': mod_name,
+                                                            'index': index, 'sortField': 1})
+            if "data" not in r:
+                break
+            
+            r = filter_on(r["data"], "name", mod_name)
+            index += 50
+
+        if isinstance(r, dict) or index + 50 > 10_000:
+            print(f"resourcepack not found : {mod_name}")
+            continue
+
+        mod = r[0]
+        mod_id = mod['id']
+        mod_name = mod['name']
+        curse_forge_link = "https://www.curseforge.com/minecraft/texture-packs/"+mod["slug"]
+
+        if not is_already_save(mod_name):
+            to_write.append(f'{mod_name}{spliter}{mod_id}{spliter}{curse_forge_link}')
+        add_to_list(mod_id, mod_name, "resourcepack")
+
+    if len(to_write) > 0:
+        write_file(to_write, data_path, "resourcepacks_id.txt")
 
 
 def setup_target_modpack_mod(loader_id):
@@ -296,48 +338,85 @@ def setup_texturepacks_id():
 
 def create_mods_pack():
     delete_folder_if_exist(modpacks_path)
-    
     os.mkdir(modpacks_path)
-    create_files_if_not_exist(modpacks_path, ["manifest.json", "modlist.html"])
 
-    manifest = {
-        "minecraft": {
-            "version": modpack_info["minecraft"]["version"],
-            "modLoaders": [
-            {
-                "id": "fabric-0.15.7",
-                "primary": True
-            }
-            ]
-        },
-        "manifestType": "minecraftModpack",
-        "manifestVersion": 1,
-        "name": modpack_info["modpack"]["name"],
-        "version": modpack_info["modpack"]["version"],
-        "author": modpack_info["modpack"]["author"],
-        "files": [],
-        "overrides": "overrides"
-    }
+    build_info = modpack_info["build"]
+    modpack_inf = modpack_info["modpack"]
+
+    modpack_name = modpack_inf["name"]
+    modpack_version = modpack_inf["version"]
+    modpack_author = modpack_inf["author"]
+
+    minecraft_version = modpack_info["minecraft"]["version"]
     
+    if build_info["curseforge-modpack-zip"] == True:
+        create_files_if_not_exist(modpacks_path, ["manifest.json", "modlist.html"])
 
-    modlist = "<ul>"
-    for file in files:
-        project_id = file[0]
-        manifest["files"].append({
-                "projectID": project_id,
-                "fileID": file[1],
-                "required": True
-            })
+        loader_version = ""
+        if loader_info[0] == "Fabric":
+            loader_version = f'fabric-{request("https://meta.fabricmc.net/v2/versions/loader/")[0]["version"]}'
+
+        manifest = {
+            "minecraft": {
+                "version": minecraft_version,
+                "modLoaders": [
+                {
+                    "id": loader_version,
+                    "primary": True
+                }
+                ]
+            },
+            "manifestType": "minecraftModpack",
+            "manifestVersion": 1,
+            "name": modpack_name,
+            "version": modpack_version,
+            "author": modpack_author,
+            "files": [],
+            "overrides": "overrides"
+        }
         
-        modlist += f"\n<li><a href=\"{get_mod_link(str(project_id))}\">{get_name_save(project_id)}</a></li>"
-    modlist += "\n</ul>"
-    
-    write_file([json.dumps(manifest, indent=1)], modpacks_path, "manifest.json")
-    write_file([modlist], modpacks_path, "modlist.html")
+        modlist = "<ul>"
+        for file in files:
+            project_id = file[0]
+            manifest["files"].append({
+                    "projectID": project_id,
+                    "fileID": file[1],
+                    "required": True
+                })
+            
+            modlist += f"\n<li><a href=\"{get_mod_link(str(project_id))}\">{get_name_save(project_id)}</a></li>"
+        modlist += "\n</ul>"
+        
+        write_file([json.dumps(manifest, indent=1)], modpacks_path, "manifest.json")
+        write_file([modlist], modpacks_path, "modlist.html")
 
-    file_name = f'{manifest["name"]}-{manifest["minecraft"]["version"]}-{manifest["version"]}'
-    delete_files_if_exist([file_name])
-    shutil.make_archive(file_name, 'zip', modpacks_path)
+        file_path = f'{modpacks_path}/curseforge'
+        os.mkdir(file_path)
+
+        shutil.make_archive(f"{file_path}/{modpack_name}-{modpack_version}", 'zip', modpacks_path)
+    
+    if build_info["classic-zip-folder"] == True:
+        default_file_path = modpacks_path+"classic/"
+        os.mkdir(default_file_path)
+        os.mkdir(default_file_path+"mods/")
+        os.mkdir(default_file_path+"resourcepacks/")
+
+        for file in files:
+            mod_id = file[0]
+            file_id = file[1]
+            file_data = request(f"{api}/mods/{mod_id}/files/{file_id}")["data"]
+
+            file_name = file_data["fileName"]
+            file_url = file_data["downloadUrl"]
+            
+            file_path = default_file_path
+            if file_name.endswith(".jar"):
+                file_path += "mods/"
+            else:
+                file_path += "resourcepacks/"
+            
+            urllib.request.urlretrieve(file_url, file_path+file_name)
+        shutil.make_archive(f"{default_file_path}/{modpack_name}-{modpack_version}", 'zip', default_file_path)
 
 
 if __name__ == "__main__":
@@ -362,6 +441,7 @@ if __name__ == "__main__":
 
     setup_target_modpack_mod(loader_info)
     setup_target_mod(mods_loader_type[loader_info[0]])
+    setup_target_resourcepacks()
     print(f'detected mods on curseforge : {len(mods_id)}')
 
     setup_mod_id()
